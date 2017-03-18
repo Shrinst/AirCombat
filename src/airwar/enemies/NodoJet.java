318bd610526e6f74c22d6632bfba7fa9ff94cbce
Shrinst <wodofwarras1@gmail.com>
2017-03-18 07:48:26 -0600

    Update and rename Jet.java to NodoJet.java
    Creacion del nodo Jet

--- src/airwar/enemies/NodoJet.java
+++ src/airwar/enemies/NodoJet.java
@@ -2,35 +2,40 @@ package airwar.enemies;
 
 import java.awt.Graphics;
 import java.util.Random;
-
 import javax.swing.ImageIcon;
 
-public class Jet {
-
-	private static int posX;
-	private static int posY;
+public class NodoJet {
+	private  int posX;
+	private  int posY;
 	public Random rnd = new Random();
 
 	public final ImageIcon img;
-
-	/**
-	 * Crea el constructor de la clase.
-	 */
-	public Jet() {
-		// Carga la imagen.
+	public NodoJet next;
+	private int pos;
+	public NodoJet(int valueX, int valueY,int tipo){
+		setNext(null);
+		this.posX = valueX;
+		this.posY= -256*valueY;
 		img = new ImageIcon(this.getClass().getResource("/airwar/images/1444842379440.png"));
-
 	}
-
+	public NodoJet getNext() {
+		return next;
+	}
+	public int getpos() {
+		return pos;
+	}
+	public void setNext(NodoJet next) {
+		this.next = next;
+	}
 	public void move() {		
 		int x = 2;
 		this.cambiarPosY(true);
+		this.cambiarPosY(true);
 		int value = rnd.nextInt(x);
-
 		if (value < (x / 2)) {
-			this.cambiarPosX(true);
-			if (this.getPosX() < 286) {
+			if (this.getPosX() < 800-32) {
 				this.cambiarPosX(true);
+				
 			}
 		} else {
 			if (this.getPosX() > 0) {
@@ -41,41 +46,40 @@ public class Jet {
 
 	public void cambiarPosY(boolean Flag) {
 		if (Flag == true) {
-			Jet.posY++;
+			this.posY++;
 
 		} else {
-			Jet.posY--;
+			this.posY--;
 		}
 	}
 
 	public void cambiarPosX(boolean Flag) {
 		if (Flag == true) {
-			Jet.posX++;
+			this.posX++;
 
 		} else {
-			Jet.posX--;
+			this.posX--;
 		}
 	}
 
 	public int getPosX() {
-		return posX;
+		return this.posX;
 	}
 
 	public int getPosY() {
-		return Jet.posY;
+		return this.posY;
 	}
 
 	public void setPosX(int x) {
-		Jet.posX = x;
+		this.posX = x;
 	}
 
 	public void setPosY(int y) {
-		Jet.posY = y;
+		this.posY = y;
 	}
-
-	// depende de la dirección de la moto carga su correspondiente imagen.
-	public void paintImage(Graphics g, int x, int y) {
-		g.drawImage(img.getImage(), x - 8, y - 50, 32, 64, null);
+	// depende de la direcciÃ³n de la moto carga su correspondiente imagen.
+	public void paintImage(Graphics g) {
+		g.drawImage(img.getImage(), this.posX, this.posY , 32, 64, null);
 	}
 
 }
