6e253b443fb31bb8570588771fcd570500f9ce31
Shrinst <wodofwarras1@gmail.com>
2017-04-02 10:28:34 -0500

    Add files via upload
    Cambio al juego

--- src/airwar2/graphics/Game.java
+++ src/airwar2/graphics/Game.java
@@ -17,10 +17,14 @@ import java.util.Random;
 
 import javax.swing.JFrame;
 
+import airwar2.datastructures.EnemyBullets;
 import airwar2.datastructures.EnemyQueue;
 import airwar2.datastructures.PlayerBullets;
+import airwar2.multithreading.ThreadPool;
 import airwar2.player.KeyInput;
 import airwar2.player.Player;
+import airwar2.playmusic.MusicPlayer;
+import airwar2.playmusic.SoundEffectPlayer;
 import airwar2.player.Bullet;
 import airwar2.spritesheet.BufferedImageLoader;
 import airwar2.spritesheet.SpriteSheet;
@@ -30,7 +34,7 @@ public class Game extends Canvas implements Runnable {
 	private static final long serialVersionUID = -3637593333786223621L;
 
 	public static final int WIDTH = 320;
-	public static final int HEIGHT = WIDTH / 12 * 9;
+	public static final int HEIGHT = 300;
 	public static final int SCALE = 2;
 	public final String NAME = "AirWar";
 	public int fps = 0;
@@ -39,70 +43,84 @@ public class Game extends Canvas implements Runnable {
 	private static int y = 0;
 	private int shipNum = 50;
 	private static String shooting = "";
-	private static boolean gamePad = true;
-	private boolean isAlive = false;
+	private static boolean gamePad = true; // Android false; PC true
+	private volatile boolean isAlive = false; // volatile is needed to make multi-threading works
+	private boolean is_shooting = false;
 
 	private Thread thread;
 	private JFrame jframe;
 	private Player player;
 	private PlayerBullets bullets;
+	private EnemyBullets enemyBullets;
 	private EnemyQueue enemyQueue;
+	private MusicPlayer musicPlayer;
 	
-	private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
 	private BufferedImage backgroundImage = null;
+	private BufferedImage iconImage = null;
 	private BufferedImage spriteSheet = null;
-	
+
 	private Random rnd = new Random();
-	
+
 	private ServerSocket serversocket;
 	private Socket socket;
 	private BufferedReader bufferedReader;
 	private BufferedWriter bufferedWriter;
-	private final int PORT = 2222;	
+	private final int PORT = 2222;
 
-	public Game() {
-		this.setUpFrame();
+	public Game() {			
+		this.setUpGame();
 		this.init();
-		this.runServer();
-		this.start();
+		this.runServer();		
+		//this.start();
 		jframe.setVisible(true);
 	}
 
-	private void setUpFrame() {
+	private void setUpGame() {
 		this.setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
-	    this.setMaximumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
-	    this.setMinimumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
-	    
+		this.setMaximumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		this.setMinimumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));	
+		
+
 		jframe = new JFrame();
 		jframe.add(this);
 		jframe.pack();
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jframe.setResizable(false);
-		jframe.setLocationRelativeTo(null);		
+		jframe.setLocationRelativeTo(null);			
 	}
-	
+
 	private void init() {
-		
+
 		BufferedImageLoader loader = new BufferedImageLoader();
-        
-        try {
-            spriteSheet = loader.loadImage("/airwar2/images/spritesheet_imagenes.png");
-            SpriteSheet spriteSheet = new SpriteSheet(this.spriteSheet);
-            backgroundImage = spriteSheet.grabImage(3, 1, 32, 32);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }   
-        
-        this.addKeyListener(new KeyInput(this));
-        requestFocus();
-        
-        player = new Player(200, 200, this);
-        bullets = new PlayerBullets();
-        enemyQueue = new EnemyQueue(this);
-        
-        for (int i = 0; i < shipNum; i++) {// cantidad enemigos
+
+		try {
+			spriteSheet = loader.loadImage("/airwar2/images/spritesheet_imagenes.png");
+			SpriteSheet spriteSheet = new SpriteSheet(this.spriteSheet);
+			backgroundImage = spriteSheet.grabImage(3, 1, 32, 32);
+			iconImage = spriteSheet.grabImage(1, 1, 32, 32);
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+		this.addKeyListener(new KeyInput(this));		
+		jframe.setIconImage(iconImage);
+
+		
+		bullets = new PlayerBullets();
+		enemyBullets = new EnemyBullets();
+		enemyQueue = new EnemyQueue(this);	
+		player = new Player(200, 200, this);
+		musicPlayer = new MusicPlayer("Wizard&Warriors", "ForestOfFellatio", "Dr.Willy");		
+
+		for (int i = 0; i < shipNum; i++) {
 			enemyQueue.insert();
 		}
+		
+		ThreadPool threadPool = new ThreadPool(2);		
+		threadPool.runTask(musicPlayer);
+		threadPool.runTask(this);
+		//threadPool.join();
 	}
 
 	private synchronized void start() {
@@ -128,110 +146,121 @@ public class Game extends Canvas implements Runnable {
 	}
 
 	private void tick() {
-		if (gamePad == false) {
-			if (x > 0) {
+		if (!gamePad) {
+			if (y > 0) {
 				player.setVelX(y);
 			}
+			if (y < 0) {
+				player.setVelX(-Math.abs(y));
+				;
+			}
+			if (x > 0) {
+				player.setVelY(x);				
+			}
 			if (x < 0) {
-				player.setVelX(-Math.abs(y));;
-			} 
-			if (y > 0) {
-				player.setVelY(-x);;
-			} 
-			if ( y < 0) {
 				player.setVelY(-Math.abs(x));
 			}
 			if (shooting.equals("shooting")) {
 				bullets.addBullet(new Bullet(player.getX(), player.getY(), this));
+				new SoundEffectPlayer("Laser_Shoot11").play(-25);
 			}
 		}
 		player.tick();
-		bullets.tick();
+		bullets.tick();	
+		enemyBullets.tick();		
 		aps++;
 	}
 
 	private void render() {
-		
+
 		BufferStrategy bufferStrategy = this.getBufferStrategy();
 		int value = rnd.nextInt(30);
-		
-        if (bufferStrategy == null) {
-            createBufferStrategy(3);
-            return;
-        }
-        
-        Graphics g = bufferStrategy.getDrawGraphics();  
-        
-        //g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
-        g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), null);  
-        player.render(g);
-        bullets.render(g);
-        if (enemyQueue.getSize() > 3 && value == 6) {
+
+		if (bufferStrategy == null) {
+			createBufferStrategy(3);
+			return;
+		}
+
+		Graphics g = bufferStrategy.getDrawGraphics();
+
+		// g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+		g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), null);
+		player.render(g);
+		bullets.render(g);
+		enemyBullets.render(g);
+
+		if (enemyQueue.getSize() > 0 && value == 6) {
 			enemyQueue.delete();
 		}
-		enemyQueue.action(g);
-                
-        g.dispose();
-        bufferStrategy.show();
+		enemyQueue.action(g, (int) player.getX(), (int) player.getY());
+
+		g.dispose();
+		bufferStrategy.show();
 		fps++;
 	}
-	
+
 	public void keyPressed(KeyEvent e) {
-		if (gamePad) {			
-			int key = e.getKeyCode();     
-	        
-	        switch (key) {
-	            case KeyEvent.VK_D:  
-	                player.setVelX(5);
-	                break;
-	            case KeyEvent.VK_A:
-	                player.setVelX(-5);
-	                break;
-	            case KeyEvent.VK_W:
-	                player.setVelY(-5);
-	                break;
-	            case KeyEvent.VK_S:
-	                player.setVelY(5);
-	                break;
-	            case KeyEvent.VK_SPACE:
-	                bullets.addBullet(new Bullet(player.getX(), player.getY(), this));
-	                break;            
-	        }
+		if (gamePad) {
+			int key = e.getKeyCode();
+
+			if (key == KeyEvent.VK_D) {
+				player.setVelX(5);
+			} else if (key == KeyEvent.VK_A) {
+				player.setVelX(-5);
+			} else if (key == KeyEvent.VK_W) {
+				player.setVelY(-5);
+			} else if (key == KeyEvent.VK_S) {
+				player.setVelY(5);
+			} else if (key == KeyEvent.VK_SPACE && !is_shooting) {
+				is_shooting = true;
+				bullets.addBullet(new Bullet(player.getX(), player.getY(), this));				
+				new SoundEffectPlayer("Laser_Shoot11").play(-10);				
+			}
 		}
 	}
-	
+
 	public void keyReleased(KeyEvent e) {
-		if (gamePad) {		
-		
-			int key = e.getKeyCode();     
-		    
-		    switch (key) {
-		        case KeyEvent.VK_D:  
-		            player.setVelX(0);
-		            break;
-		        case KeyEvent.VK_A:
-		            player.setVelX(0);
-		            break;
-		        case KeyEvent.VK_W:
-		            player.setVelY(0);
-		            break;
-		        case KeyEvent.VK_S:
-		            player.setVelY(0);
-		            break;            
-		    }
-		}       
+		if (gamePad) {
+			int key = e.getKeyCode();
+			if (key == KeyEvent.VK_D) {
+				player.setVelX(0);
+			} else if (key == KeyEvent.VK_A) {
+				player.setVelX(0);
+			} else if (key == KeyEvent.VK_W) {
+				player.setVelY(0);
+			} else if (key == KeyEvent.VK_S) {
+				player.setVelY(0);
+			} else if (key == KeyEvent.VK_SPACE) {
+				is_shooting = false;
+			}
+		}
 	}
-	
+
 	public BufferedImage getSpriteSheet() {
 		return this.spriteSheet;
 	}
 	
+	public EnemyBullets getEnemyBullets() {
+		return this.enemyBullets;
+	}
+	
+	public EnemyQueue getEnemyQueue() {
+		return this.enemyQueue;
+	}
+	
+	public Player getPlayer() {
+		return this.player;
+	}
+	public PlayerBullets getPlayerBullets() {
+		return this.bullets;
+	}
+
 	private void runServer() {
 		new Thread(new Runnable() {
-			public void run() {				
-				
-				while (isAlive) {										
-					
+			public void run() {
+
+				while (isAlive) {
+
 					try {
 						System.out.println("Server Starting at Port Number: " + PORT);
 						serversocket = new ServerSocket(PORT);
@@ -240,11 +269,10 @@ public class Game extends Canvas implements Runnable {
 						System.out.println("Waiting for Clients to Connect...");
 						socket = serversocket.accept();
 						System.out.println("A Client has Connected");
-						
 
-						while (isAlive) {	
-							
-							reciveMessage();				
+						while (isAlive) {
+
+							reciveMessage();
 
 							sendMessageToClient(String.valueOf(x) + ";" + String.valueOf(y) + ";" + shooting);
 
@@ -256,14 +284,14 @@ public class Game extends Canvas implements Runnable {
 
 					} catch (IOException e) {
 						e.printStackTrace();
-					}	
-									
+					}
+
 				}
-				
+
 			}
 		}).start();
 	}
-	
+
 	private void sendMessageToClient(String message) {
 		try {
 			bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
@@ -275,7 +303,7 @@ public class Game extends Canvas implements Runnable {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void reciveMessage() {
 		String data;
 		String[] split;
@@ -287,22 +315,24 @@ public class Game extends Canvas implements Runnable {
 			if (data != null) {
 				x = Integer.valueOf(split[0]);
 				y = Integer.valueOf(split[1]);
-				shooting = split[2];										
-			}			
+				shooting = split[2];
+			}
 
 		} catch (IOException e) {
 			System.out.println("Error. Reciving the Message Failed");
-			e.printStackTrace();			
+			e.printStackTrace();
 		}
 	}
 
 	@Override
 	public void run() {
+		isAlive = true;
+		requestFocus();
 		// Initialize the variable to limit the loop´s max speed
 		final int NS_PER_SECOND = 1000000000; // How many nanoseconds are there
 												// in a second
 		final byte APS = 60; // Actualization Per Second
-		final double NS_PER_ACTUALIZATION = NS_PER_SECOND / APS; 
+		final double NS_PER_ACTUALIZATION = NS_PER_SECOND / APS;
 
 		long updateReference = System.nanoTime(); // Reference before the loop
 													// starts
@@ -330,7 +360,7 @@ public class Game extends Canvas implements Runnable {
 			}
 
 			// See the information about the FPS and APS
-			if ((System.nanoTime() - counterReference) > NS_PER_SECOND) { 
+			if ((System.nanoTime() - counterReference) > NS_PER_SECOND) {
 
 				jframe.setTitle(NAME + " || APS: " + aps + " || FPS: " + fps);
 				aps = 0;
