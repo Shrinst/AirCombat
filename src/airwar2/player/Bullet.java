2abcaf386b0de326240a7b2fbd44f69b86265420
romux09 <romux09@gmail.com>
2017-04-04 18:33:44 -0600

    Add files via upload
    Se agregaron las modificaciones a la bala

--- src/airwar2/player/Bullet.java
+++ src/airwar2/player/Bullet.java
@@ -1,72 +1,101 @@
 package airwar2.player;
 
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
 import airwar2.datastructures.EnemyList;
 import airwar2.datastructures.PlayerBullets;
+import airwar2.datastructures.PowerUpsList;
 import airwar2.enemies.NodeJet;
 import airwar2.graphics.Game;
+import airwar2.powerups.PowerUps;
 import airwar2.spritesheet.SpriteSheet;
 
 public class Bullet {
-	
+
 	private double x;
-    private double y;
-    
-    private BufferedImage image;
-    
-    private Bullet next;
-    
-    private PlayerBullets playerBullet; 
-    private Rectangle playerBulletRectangle;
-    private EnemyList enemyList;
-    
-    public Bullet(double x, double y, Game game) {
-    	this.next = null;
-        this.x = x;
-        this.y = y;      
-        
-        this.enemyList = game.getEnemyQueue().getEnemyList();
-        this.playerBullet = game.getPlayerBullets();
-        SpriteSheet spriteSheet = new SpriteSheet(game.getSpriteSheet());
-        this.image = spriteSheet.grabImage(2, 1, 32, 32);     
-        
-        
-    }   
-    
-    public void tick() {
-        this.y -= 10;  
-        playerBulletRectangle = new Rectangle((int) x+8, (int) y-4, 16, 16);
-        NodeJet temp = enemyList.getHead();
-        for (int i = 0; i < enemyList.getSize(); i++) {
-        	if (playerBulletRectangle.intersects(temp.getBounds())){
-        		if(this.y <= temp.getPosY()){
-        			enemyList.delete(temp.getPosY());
-        			playerBullet.removeBullet(this);
-        			
-        		}
-        	}
-        	temp = temp.getNext();
-        }      
-    }
-    
-    public void render(Graphics g) {
-        g.drawImage(image, (int) x + 8, (int) y - 4, 16, 16, null);
-        g.setColor(Color.red);
-    }
-    
-    public double getY() {
-        return this.y;
-    }
-    
-    public Bullet getNext() {
+	private double y;
+	private int gameScore;
+	private int type;
+
+	private BufferedImage genericImage;
+	private BufferedImage misilImage;
+
+	private Bullet next;
+	private Game game;
+
+	private Rectangle playerBulletRectangle;
+	private EnemyList enemyList;
+	private PlayerBullets playerBullet;
+	private PowerUpsList powerUpsList;
+	private Player player;
+
+	public Bullet(double x, double y, int type, Game game) {
+		this.next = null;
+		this.x = x;
+		this.y = y;
+		this.type = type;
+		this.game = game;
+		this.powerUpsList = game.getPowerUpsList();
+		this.gameScore = this.game.getScore();
+
+		this.enemyList = this.game.getEnemyQueue().getEnemyList();
+		this.playerBullet = this.game.getPlayerBullets();
+		this.player = game.getPlayer();
+
+		SpriteSheet spriteSheet = new SpriteSheet(this.game.getSpriteSheet());
+		this.genericImage = spriteSheet.grabImage(2, 1, 32, 32);
+		this.misilImage = spriteSheet.grabImage(3, 1, 32, 32);
+
+	}
+
+	public void tick() {
+		this.y -= 10;
+		playerBulletRectangle = new Rectangle((int) x + 8, (int) y - 4, 16, 16);
+		NodeJet temp = enemyList.getHead();
+		for (int i = 0; i < enemyList.getSize(); i++) {
+			if (playerBulletRectangle.intersects(temp.getBounds())) {
+				if (this.y <= temp.getPosY()) {
+					enemyList.delete(temp.getPosY());
+					playerBullet.removeBullet(this);
+					game.setScore(gameScore += 10);
+				}
+				if (Math.random() <= 0.30) {
+					powerUpsList.addPowerUp(new PowerUps(temp.getPosX(), temp.getPosY(), 1, game));
+				}
+			}
+			temp = temp.getNext();
+		}
+	}
+
+	public void render(Graphics g) {
+		if (type == 0) {
+			g.drawImage(genericImage, (int) x + 8, (int) y - 4, 16, 16, null);
+		}
+		if (type == 1) {
+			g.drawImage(misilImage, (int) x + 8, (int) y - 4, 16, 16, null);
+		}
+		//System.out.println(this.type);
+	}
+	
+	public int getType() {
+		return this.type;
+	}
+	
+	public void setType(int type) {
+		this.type = type;
+	}
+	
+	public double getY() {
+		return this.y;
+	}
+
+	public Bullet getNext() {
 		return this.next;
 	}
-    
-    public void setNext(Bullet next) {
-    	this.next = next;
-    }
+
+	public void setNext(Bullet next) {
+		this.next = next;
+	}
 }
--- src/airwar2/player/Player.java
+++ src/airwar2/player/Player.java
@@ -5,9 +5,12 @@ import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
 import airwar2.datastructures.EnemyList;
+import airwar2.datastructures.PowerUpsList;
+import airwar2.datastructures.Stack;
 import airwar2.enemies.NodeJet;
 import airwar2.graphics.Game;
 import airwar2.playmusic.SoundEffectPlayer;
+import airwar2.powerups.PowerUps;
 import airwar2.spritesheet.SpriteSheet;
 
 public class Player {
@@ -21,16 +24,21 @@ public class Player {
 	private BufferedImage player;
 
 	private Rectangle playerRectangle;
-
 	private EnemyList enemyList;
+	private PowerUpsList powerUpsList;
+	private Game game;
+	private Stack stack;
 
 	public Player(double x, double y, Game game) {
 		this.x = x;
 		this.y = y;
 
-		this.enemyList = game.getEnemyQueue().getEnemyList();
+		this.game = game;
+		this.enemyList = this.game.getEnemyQueue().getEnemyList();
+		this.powerUpsList = this.game.getPowerUpsList();
+		this.stack = this.game.getStack();
 
-		SpriteSheet spriteSheet = new SpriteSheet(game.getSpriteSheet());
+		SpriteSheet spriteSheet = new SpriteSheet(this.game.getSpriteSheet());
 		this.player = spriteSheet.grabImage(1, 1, 32, 32);
 	}
 
@@ -38,7 +46,7 @@ public class Player {
 		x += velX;
 		y += velY;
 
-		playerRectangle = this.getBounds();
+		this.playerRectangle = this.getBounds();
 
 		if (x <= 0) {
 			x = 0;
@@ -52,26 +60,44 @@ public class Player {
 		if (y >= 610 - 32) {
 			y = 610 - 32;
 		}
+		collideWithEnemy();
+		collideWithPowerUp();
+	}
 
+	private void collideWithEnemy() {
 		NodeJet temp = enemyList.getHead();
 		for (int i = 0; i < enemyList.getSize(); i++) {
-
 			if (playerRectangle.intersects(temp.getBounds())) {
 				if (this.y <= temp.getPosY()) {
 					if (temp.getType() < 5) {
-						enemyList.delete(temp.getPosY());						
 						new SoundEffectPlayer("Allahu_Akbar").play(-10);
-					} else {
-						enemyList.delete(temp.getPosY());
 					}
+					enemyList.delete(temp.getPosY());
 				}
 			}
 			temp = temp.getNext();
 		}
 	}
 
+	public void collideWithPowerUp() {
+		PowerUps temp = powerUpsList.getHead();
+		for (int i = 0; i < powerUpsList.getSize(); i++) {
+			if (temp != null) {
+				if (playerRectangle.intersects(temp.getBounds())) {
+					if (this.y <= temp.getPosY()) {
+						powerUpsList.removePowerUp(temp.getPosY());
+						game.pushStack(temp, temp.getType());						
+						temp = temp.getNext();
+					}
+				}
+			} else {
+				break;
+			}
+		}
+	}
+
 	public void render(Graphics g) {
-		g.drawImage(this.player, (int) this.x, (int) this.y, null);
+		g.drawImage(this.player, (int) this.x, (int) this.y, null);		
 	}
 
 	public double getX() {
