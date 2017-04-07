8467c8ea148ba0e21a0670f80c9e979ebdad846f
Ema0898 <aguisnchez@gmail.com>
2017-04-07 05:33:59 -0600

    Android
    Agrega el boton de power ups

---
+++ main/java/com/example/aguis/airwarcontroller20/Menu.java
@@ -0,0 +1,28 @@
+package com.example.aguis.airwarcontroller20;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+
+    public void changeToMainActivity(View view) {
+        Intent intent = new Intent(Menu.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    public void changeToInstructions(View view) {
+        Intent intent = new Intent(Menu.this, Instructions.class);
+        startActivity(intent);
+    }
+}
